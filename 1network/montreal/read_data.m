3a6bdc9ed0a2342696175d12cfeb59f329d9bf49
wangshuqi1019 <crystal.sq.wang@gmail.com>
2024-03-01 17:09:26 +0800

    delete duplications

--- 1network/montreal/read_data.m
+++
@@ -1,7 +0,0 @@
-%% Define structures, including various types of data in data files
-[~, ~, neighs] = xlsread('neighs.xlsx','Sheet1');
-neighs(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),neighs)) = {''};
-
-Data.neighs=neighs;
-
-save('Data.mat','Data');
